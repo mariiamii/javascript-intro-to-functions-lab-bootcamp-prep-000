df5e26aabfeefa043712a2ef498418613d6937b2
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-03 19:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(HELLO) {
   console.log(HELLO.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  return string.toUpperCase()
+function sayHiToGrandma(hello) {
+  return hello.toUpperCase()
 }
 
