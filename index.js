a0a7eedb8be58968f91b137ddcd652e9bf104c59
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-03 18:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,5 @@ function shout(string) {
 }
 
 function whisper(string) {
-  
+  return string.toLowerCase()
 }
-
-
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
