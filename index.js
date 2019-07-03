6b3138bfcb04d57d6e86c75f0ac844dd2fc9d400
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-03 18:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,11 +9,3 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase)
 }
-
-
-
-
-
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
