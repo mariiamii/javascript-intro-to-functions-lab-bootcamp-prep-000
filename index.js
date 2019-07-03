075070be532a484a0371d2b9e46c9b7b78f7144c
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-03 18:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function logWhisper(hello) {
   console.log(hello.toLowerCase())
 }
 
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
