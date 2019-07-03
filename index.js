ff932edf76ff06c51587079375d60a2666ba0115
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-03 18:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,14 +16,3 @@ const spy = expect.spyOn(console, 'log').andCallThrough()
 
     expect(spy).toHaveBeenCalledWith('HELLO')
     
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
